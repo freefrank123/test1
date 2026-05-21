d04e27305ed24b2442f5a91ccf14692820af8185
Zihan Wang <2575788622@qq.com>
2026-05-21 23:28:05 +0800

    修改暴露的secret-key

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,6 +1,5 @@
 // Supabase 配置
-// 替换为你的实际 Supabase 项目信息
 window.SUPABASE_CONFIG = {
-  url: 'https://bpsdnnwxsgmkzmlfqmya.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwc2Rubnd4c2dta3ptbGZxbXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMzkzMDcsImV4cCI6MjA5NDkxNTMwN30.eJYbF2tEsO3wJUj9O-klj58UweiSiS74masenUdtYGM'
+  url: 'https://ooqhfpbiqrkxfrvdckik.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vcWhmcGJpcXJreGZydmRja2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNjg2OTQsImV4cCI6MjA5NDk0NDY5NH0.j3hu3VVoBsgfpxfzVCKUuUV5cudPubDUXVL9Kk84CzI'
 };
